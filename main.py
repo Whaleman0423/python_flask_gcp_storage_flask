eab810587b325cde0dd8b5b3c47fec06c0e58ba7
Whaleman0423 <sheiyuray@gmail.com>
2022-06-19 14:34:04 +0800

    remove references

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
 from flask import Flask
 
-# 最簡單的 flask 展示
-# 連結根路徑會跟你說 Hello, world
-
-# 建立 Flask 物件
 app = Flask(__name__)
 
-# 定義根路由
 @app.route('/')
 def hello_world():
     return 'Hello, world'
